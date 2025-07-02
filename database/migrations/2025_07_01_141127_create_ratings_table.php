4d9749a6f266e5efe38680e7bbbac16fa652458e
Arun Rajan <arun.rajan@jadu.net>
2025-07-02 18:29:19 +0100

    Added migrations for tables: turbines, components, turbine_component & ratings

---
+++ database/migrations/2025_07_01_141127_create_ratings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('turbine_id')->constrained()->onDelete('cascade');
+            $table->foreignId('component_id')->constrained()->onDelete('cascade');
+            $table->tinyInteger('rating')->unsigned();
+            $table->dateTime('inspected_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ratings');
+    }
+};
