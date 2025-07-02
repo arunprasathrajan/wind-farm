5f832f18276b1a5245ebc7d4c52a5bcec9fcedeb
Arun Rajan <arun.rajan@jadu.net>
2025-07-02 19:19:08 +0100

    Added validation for date

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\InspectorController;
+use App\Http\Controllers\RatingController;
+use App\Http\Controllers\TurbineController;
 use Illuminate\Support\Facades\Route;
 
 /*
